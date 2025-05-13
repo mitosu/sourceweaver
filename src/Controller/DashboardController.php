a95fbc0593355283a23438826625e4a02639bc15
Miguel Angel <miguelangel.torres@outlook.com>
2025-05-13 13:09:13 +0200

    feat: sidebar changes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -11,8 +11,28 @@ final class DashboardController extends AbstractController
     #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
+        $workspaces = [
+            [
+                'name' => 'MCEI - Administración',
+                'id' => 1,
+                'routes' => [
+                    ['label' => 'Overview', 'icon' => 'bi-grid', 'path' => 'workspace_overview', 'params' => ['id' => 1]],
+                    ['label' => 'Documentos', 'icon' => 'bi-file-earmark-text', 'path' => 'workspace_documents', 'params' => ['id' => 1]],
+                ]
+            ],
+            [
+                'name' => 'Software&Development',
+                'id' => 2,
+                'routes' => [
+                    ['label' => 'Overview', 'icon' => 'bi-grid', 'path' => 'workspace_overview', 'params' => ['id' => 2]],
+                    ['label' => 'Tickets', 'icon' => 'bi-life-preserver', 'path' => 'workspace_tickets', 'params' => ['id' => 2]],
+                ]
+            ],
+        ];
+        
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'workspaces' => $workspaces
         ]);
     }
 }
